446322da737de31d6b5cb4ecdbdf0750ebc01371
julien93140 <julien.moratalla@gmail.com>
2019-09-28 13:51:21 +0200

    test

--- js/controller.js
+++ js/controller.js
@@ -1,20 +1,33 @@
-app.controller("myCtrl", function($scope) {
+app.controller("myCtrl", function($scope, $log, $filter) {
+    // Variables
     $scope.myArray = [
         {
+            id: 1,
             type: "ZONE",
             value: "Zone Analytics"
         },
         {
+            id: 2,
             type: "HOME",
             value: "Home Analytics"
         },
         {
+            id: 3,
             type: "TEST",
             value: "Test Analytics"
         }
     ];
 
-    $scope.exportCsv = (type) => {
-        console.log(type)
+    $scope.test = "";
+    $scope.myDate = "";
+
+    $scope.exportCsv = (type, date) => {
+        let dateFormated = $filter('date')(date, 'dd/MM/yyyy');
+        $log.log(type, dateFormated)
+    }
+
+
+    $scope.testFunc = (test) => {
+        $log.log(test)
     }
 });
