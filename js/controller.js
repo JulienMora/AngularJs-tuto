161bb5b42c4c390bcbb4204d277a74cc27fcb4bb
julien93140 <julien.moratalla@gmail.com>
2019-09-28 14:26:13 +0200

    test

--- js/controller.js
+++ js/controller.js
@@ -1,29 +1,52 @@
 app.controller("myCtrl", function($scope, $log, $filter) {
+    
     // Variables
     $scope.myArray = [
         {
             id: 1,
             type: "ZONE",
+            dateStart: "",
+            dateEnd: "",
             value: "Zone Analytics"
         },
         {
             id: 2,
             type: "HOME",
+            dateStart: "",
+            dateEnd: "",
             value: "Home Analytics"
         },
         {
             id: 3,
             type: "TEST",
+            dateStart: "",
+            dateEnd: "",
             value: "Test Analytics"
         }
     ];
 
-    $scope.test = "";
-    $scope.myDate = "";
+    $scope.dateStart = "";
+    $scope.dateEnd = "";
 
-    $scope.exportCsv = (type, date) => {
-        let dateFormated = $filter('date')(date, 'dd/MM/yyyy');
-        $log.log(type, dateFormated)
+
+    //Methods
+    $scope.exportCsv = (type, dateStart, dateEnd) => {
+        let dateStartFormated = $filter('date')(dateStart, 'yyyy/MM/dd');
+        let dateEndFormated = $filter('date')(dateEnd, 'yyyy/MM/dd');
+
+        let dateStartCalc =  new Date(dateStartFormated);
+        let dateEndCalc =  new Date(dateEndFormated);
+
+        let diffTime = Math.abs(dateEndCalc -  dateStartCalc);
+        let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+        $log.log("Type : " + type, "Date Start : " +  dateStartFormated, "Date End : " +  dateEndFormated);
+        
+        if ( diffDays >= 0 ) {
+            $log.log("DateDiff : " + diffDays);
+        } else {
+            $log.log("Erreur de formatage entre les dates");
+        }
     }
 
 
