ea0a17835de2f4deed9a4cef6e94ec30d97f4903
julien93140 <julien.moratalla@gmail.com>
2019-10-07 21:27:04 +0200

    chartjs

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,4 @@
-app.controller("myCtrl", function($scope, $log, $filter, serviceExample) {
+app.controller("myCtrl", function($scope, $log, $filter, serviceExample, $document) {
 
     // Variables
     $scope.myArray = [
@@ -51,25 +51,25 @@ app.controller("myCtrl", function($scope, $log, $filter, serviceExample) {
         let diffTime = Math.abs(dateEndCalc -  dateStartCalc);
         let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
-        $log.log("Type : " + type, "Date Start : " +  dateStartFormated, "Date End : " +  dateEndFormated);
+        //$log.log("Type : " + type, "Date Start : " +  dateStartFormated, "Date End : " +  dateEndFormated);
         
         if ( diffDays >= 0 ) {
             // Make the export
-            $log.log("DateDiff : " + diffDays);
+           //$log.log("DateDiff : " + diffDays);
 
             //Test service 
             serviceExample.service();
         } else {
-            $log.log("Erreur de formatage entre les dates");
+            //$log.log("Erreur de formatage entre les dates");
         }
     };
 
     $scope.setDateRange = (type) => {
         $log.log("click on setDateRangeFunc");
         $scope.myArray.map(function(el) {
-            console.log(el);
+            //console.log(el);
             if (el.type === type) {
-                console.log(type)
+                //console.log(type)
                 el.rangePicker.start = "00/00/0000";
                 el.rangePicker.end = "11/11/1111";
             }
@@ -78,7 +78,7 @@ app.controller("myCtrl", function($scope, $log, $filter, serviceExample) {
 
     var prettifyPeriods = (model) => {
         model.map(function(el) {
-            console.log(el)
+            //console.log(el)
             if (el.rangePicker) {
                 Object.assign(el, {"prettifyPeriod": el.rangePicker.start + " to " + el.rangePicker.end});
             }
@@ -87,9 +87,56 @@ app.controller("myCtrl", function($scope, $log, $filter, serviceExample) {
 
     prettifyPeriods($scope.myArray);
 
-    console.log($scope.myArray);
+    //console.log($scope.myArray);
 
     $scope.testFunc = (test) => {
         $log.log(test)
     };
+
+    /*
+    *   Chartjs 
+    */
+
+    /*
+
+    $scope.chartjsDatas = {
+        datas: {
+            labels: ["Tokyo",	"Mumbai",	"Mexico City",	"Shanghai",	"Sao Paulo",	"New York",	"Karachi","Buenos Aires",	"Delhi","Moscow"],
+            datasets: [{
+                label: 'Series 1', // Name the series
+                data: [500,	50,	2424,	14040,	14141,	4111,	4544,	47,	5555, 6811], // Specify the data values array
+                fill: false,
+                borderColor: '#2196f3', // Add custom color border (Line)
+                backgroundColor: '#2196f3', // Add custom color background (Points and Fill)
+                borderWidth: 1 // Specify bar border width
+            },
+                    {
+                label: 'Series 2', // Name the series
+                data: [1288,	88942,	44545,	7588,	99,	242,	1417,	5504,	75, 457], // Specify the data values array
+                fill: false,
+                borderColor: '#4CAF50', // Add custom color border (Line)
+                backgroundColor: '#4CAF50', // Add custom color background (Points and Fill)
+                borderWidth: 1 // Specify bar border width
+            }]
+        },
+        options: {
+            responsive: true, // Instruct chart js to respond nicely.
+            maintainAspectRatio: false, // Add to prevent default behaviour of full-width/height 
+        }
+    };
+        
+
+    let init = () => {
+        var ctx = angular.element(document.getElementById("myChart").getContext('2d'));
+
+        new Chart(ctx, {
+            type: 'line',
+            data: $scope.chartjsDatas.datas,
+            options: $scope.chartjsDatas.options
+        });
+    };
+
+    init();
+    
+*/
 });
