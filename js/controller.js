c7ac85596df66d48912c332ede780b9810e13678
julien93140 <julien.moratalla@gmail.com>
2019-10-07 21:40:36 +0200

    chartjs

--- js/controller.js
+++ js/controller.js
@@ -96,47 +96,4 @@ app.controller("myCtrl", function($scope, $log, $filter, serviceExample, $docume
     /*
     *   Chartjs 
     */
-
-    /*
-
-    $scope.chartjsDatas = {
-        datas: {
-            labels: ["Tokyo",	"Mumbai",	"Mexico City",	"Shanghai",	"Sao Paulo",	"New York",	"Karachi","Buenos Aires",	"Delhi","Moscow"],
-            datasets: [{
-                label: 'Series 1', // Name the series
-                data: [500,	50,	2424,	14040,	14141,	4111,	4544,	47,	5555, 6811], // Specify the data values array
-                fill: false,
-                borderColor: '#2196f3', // Add custom color border (Line)
-                backgroundColor: '#2196f3', // Add custom color background (Points and Fill)
-                borderWidth: 1 // Specify bar border width
-            },
-                    {
-                label: 'Series 2', // Name the series
-                data: [1288,	88942,	44545,	7588,	99,	242,	1417,	5504,	75, 457], // Specify the data values array
-                fill: false,
-                borderColor: '#4CAF50', // Add custom color border (Line)
-                backgroundColor: '#4CAF50', // Add custom color background (Points and Fill)
-                borderWidth: 1 // Specify bar border width
-            }]
-        },
-        options: {
-            responsive: true, // Instruct chart js to respond nicely.
-            maintainAspectRatio: false, // Add to prevent default behaviour of full-width/height 
-        }
-    };
-        
-
-    let init = () => {
-        var ctx = angular.element(document.getElementById("myChart").getContext('2d'));
-
-        new Chart(ctx, {
-            type: 'line',
-            data: $scope.chartjsDatas.datas,
-            options: $scope.chartjsDatas.options
-        });
-    };
-
-    init();
-    
-*/
 });
